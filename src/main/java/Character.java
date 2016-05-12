d57d086a0376965af486591447c68280aec64df5
WinnieHsu <s101021122@m101.nthu.edu.tw>
2016-05-12 19:05:35 +0800

    <add main circle and load data>

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,9 +1,8 @@
 package main.java;
 
 import java.util.ArrayList;
-import java.util.Random;
 import processing.core.PApplet;
-import processing.event.MouseEvent;
+
 
 /**
 * This class is used to store states of the characters in the program.
@@ -11,32 +10,48 @@ import processing.event.MouseEvent;
 */
 public class Character {
 	
+	public float x, y, radius=30;
+	private float initial_x, initial_y;
 	private MainApplet parent;
-	public float x, y, radius;
+	private MainApplet border;
 	private String name;
-	//private PApplet parent;
 	private ArrayList<Character> targets;
-
-	public Character(MainApplet parent, String name, float x, float y){
-		
+	private boolean mouse_in = false;
+	
+	public Character(MainApplet parent, String name, float x, float y) {
 		this.parent = parent;
-		this.name = name;
+		this.name = name; 
+		this.initial_x = x;
+		this.initial_y = y;
 		this.x = x;
 		this.y = y;
 		targets = new ArrayList<Character>();
 	}
-
-	public void display(){
-		
-		parent.ellipse(this.x, this.y, 50, 20);
-		parent.text(this.name, this.x-15, this.y-15);
+	
+	public void display() {
+		parent.fill(123, 0, 200);
+		parent.ellipse(x, y, radius, radius);
+		if (mouse_in == true) {
+			border.stroke(48, 5, 180);
+			border.ellipse(x, y, radius, radius);
+			parent.text(name, x, y);
+		}
 	}
 	
-	public void addTarget(Character target){
+	public void addTarget(Character target) {
 		this.targets.add(target);
 	}
 	
-	public ArrayList<Character> getTargets(){
+	public ArrayList<Character> getTargets() {
 		return this.targets;
 	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setMouseIn() {
+		mouse_in = true;
+	}
+	
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -12,22 +12,40 @@ import controlP5.*;
 * You can do major UI control and some visualization in this class.  
 */
 @SuppressWarnings("serial")
-public class MainApplet extends PApplet{
+public class MainApplet extends PApplet {
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
+	private String address = path.concat(file);
 	
 	JSONObject data;
 	JSONArray nodes, links;
 	private ArrayList<Character> characters;
 	private ControlP5 cp5; 
-	
-	
+	private int epsNum = 1;
+	private int num_in_great_circle = 0;
+	private float circle_x, circle_y, radius;
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
 
 		size(width, height);
+		circle_x = width / 2;
+		circle_y = height / 2;
+		radius = (width-50)/2;
+		background(255);
+		//text("Star Wars 1", width/3, 5);
+		cp5 = new ControlP5(this);
 		
+		cp5.addButton("addAll")
+			.setLabel("ADD ALL")
+			.setColorBackground(139)
+			.setPosition(width*3/4, height/20)
+			.setSize(100, 30);
+		cp5.addButton("clear")
+			.setLabel("CLEAR")
+			.setColorBackground(139)
+			.setPosition(width*3/4, 80+height/20)
+			.setSize(100, 30);
 		
 		characters = new ArrayList<Character>();
 		smooth();
@@ -37,18 +55,22 @@ public class MainApplet extends PApplet{
 
 	public void draw() {
 		background(255);
-		for(Character character:characters){
-			fill(0);
+		stroke(48, 139, 206);
+		//strokeWeight(5); 
+		text("Star Wars %d", width/3, 5, epsNum);
+		ellipse(circle_x, circle_y, radius, radius);
+		
+		for(Character character:characters) {
 			fill(0, 102, 153);
 			character.display();
 		}
 		
 		float src_x, src_y, dest_x, dest_y;
-		for(int i=0; i<characters.size(); i++){
+		for(int i=0; i<characters.size(); i++) {
 			src_x = characters.get(i).x;
 			src_y = characters.get(i).y;
 			
-			for(int j=0; j<characters.get(i).getTargets().size(); j++){
+			for(int j=0; j<characters.get(i).getTargets().size(); j++) {
 				dest_x = characters.get(i).getTargets().get(j).x;
 				dest_y = characters.get(i).getTargets().get(j).y;
 				fill(255);
@@ -57,42 +79,52 @@ public class MainApplet extends PApplet{
 		}
 	}
 	
-	public void keyPressed(){
+	public void keyPressed() {
 		background(255);
 		
-		for(int i=0; i<characters.size(); i++){
-			Random x = new Random();
-			Random y = new Random();
-			
-			characters.get(i).x = x.nextFloat()*800;
-			characters.get(i).y = y.nextFloat()*800;
+		Random random = new Random();
+		epsNum = random.nextInt(8);
+		while(epsNum==0){
+			epsNum = random.nextInt(8);
 		}
-	}
-
-	private void loadData(){
-		/*int j=0;
 		String num = "";
-		while(j<7){
-		j++;
-		num = num.valueOf(j);
+		num = num.valueOf(epsNum);
 		file = "starwars-episode-";
 		file = file.concat(num);
-		file = file.concat("-interactions.json");*/
-		data = loadJSONObject(file);
+		file = file.concat("-interactions.json");
+		
+	}
+	
+	public void addInCircle() {
+		
+	}
+	
+	public void mousePressed() {
+		for(Character character:characters) {
+			if (dist(character.x, character.y, mouseX, mouseY) <= character.radius) {
+				character.x = mouseX;
+				character.y = mouseY;
+			}
+		}
+	}
+	
+	public void mouseDragger() {
+
+	}
+	
+	private void loadData(){
+		int x=0, y=0;
+		data = loadJSONObject(address);
 		nodes = data.getJSONArray("nodes");
 		for(int i=0; i<nodes.size(); i++){
 			JSONObject node = nodes.getJSONObject(i);
 			
 			String name = node.getString("name");
 			int value = node.getInt("value");
-			//String colour = node.getString("colour");
 			
-			//System.out.println(i+", "+name+", "+value+", "+colour);
-			
-			Random x = new Random();
-			Random y = new Random();
-			
-			Character input = new Character(this, name, x.nextFloat()*800, y.nextFloat()*800);
+			Character input = new Character(this, name, 10+(x+5)*60, 10+(y+5)*60);
+			x = (x+1)%3;
+			y = (y+1)%9;
 			this.characters.add(input);
 		}
 		
@@ -104,12 +136,8 @@ public class MainApplet extends PApplet{
 			int target = link.getInt("target");
 			int value = link.getInt("value");
 			
-			//System.out.println(j+", "+source+", "+target+", "+value);
-			//System.out.println("target is "+characters.get(source));
-			
 			characters.get(source).addTarget( characters.get(target) );
 		}
-		//}
 	}
 
 }
