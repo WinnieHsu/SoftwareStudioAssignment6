100ce06e88a92563a5bb1319c428e26cdce443af
WinnieHsu <s101021122@m101.nthu.edu.tw>
2016-05-12 17:18:43 +0800

    <test>

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,6 +1,9 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Random;
 import processing.core.PApplet;
+import processing.event.MouseEvent;
 
 /**
 * This class is used to store states of the characters in the program.
@@ -9,15 +12,31 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
+	public float x, y, radius;
+	private String name;
+	//private PApplet parent;
+	private ArrayList<Character> targets;
 
-	public Character(MainApplet parent){
-
-		this.parent = parent;
+	public Character(MainApplet parent, String name, float x, float y){
 		
+		this.parent = parent;
+		this.name = name;
+		this.x = x;
+		this.y = y;
+		targets = new ArrayList<Character>();
 	}
 
 	public void display(){
-
+		
+		parent.ellipse(this.x, this.y, 50, 20);
+		parent.text(this.name, this.x-15, this.y-15);
 	}
 	
+	public void addTarget(Character target){
+		this.targets.add(target);
+	}
+	
+	public ArrayList<Character> getTargets(){
+		return this.targets;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,6 +1,11 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Random;
 import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+import controlP5.*;
 
 /**
 * This class is for sketching outcome using Processing
@@ -11,22 +16,100 @@ public class MainApplet extends PApplet{
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
 	
+	JSONObject data;
+	JSONArray nodes, links;
+	private ArrayList<Character> characters;
+	private ControlP5 cp5; 
+	
+	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
 
 		size(width, height);
+		
+		
+		characters = new ArrayList<Character>();
 		smooth();
 		loadData();
 		
 	}
 
 	public void draw() {
-
+		background(255);
+		for(Character character:characters){
+			fill(0);
+			fill(0, 102, 153);
+			character.display();
+		}
+		
+		float src_x, src_y, dest_x, dest_y;
+		for(int i=0; i<characters.size(); i++){
+			src_x = characters.get(i).x;
+			src_y = characters.get(i).y;
+			
+			for(int j=0; j<characters.get(i).getTargets().size(); j++){
+				dest_x = characters.get(i).getTargets().get(j).x;
+				dest_y = characters.get(i).getTargets().get(j).y;
+				fill(255);
+				line(src_x, src_y, dest_x, dest_y);
+			}
+		}
+	}
+	
+	public void keyPressed(){
+		background(255);
+		
+		for(int i=0; i<characters.size(); i++){
+			Random x = new Random();
+			Random y = new Random();
+			
+			characters.get(i).x = x.nextFloat()*800;
+			characters.get(i).y = y.nextFloat()*800;
+		}
 	}
 
 	private void loadData(){
-
+		/*int j=0;
+		String num = "";
+		while(j<7){
+		j++;
+		num = num.valueOf(j);
+		file = "starwars-episode-";
+		file = file.concat(num);
+		file = file.concat("-interactions.json");*/
+		data = loadJSONObject(file);
+		nodes = data.getJSONArray("nodes");
+		for(int i=0; i<nodes.size(); i++){
+			JSONObject node = nodes.getJSONObject(i);
+			
+			String name = node.getString("name");
+			int value = node.getInt("value");
+			//String colour = node.getString("colour");
+			
+			//System.out.println(i+", "+name+", "+value+", "+colour);
+			
+			Random x = new Random();
+			Random y = new Random();
+			
+			Character input = new Character(this, name, x.nextFloat()*800, y.nextFloat()*800);
+			this.characters.add(input);
+		}
+		
+		links = data.getJSONArray("links");
+		for(int j=0; j<links.size(); j++){
+			JSONObject link = links.getJSONObject(j);
+			
+			int source = link.getInt("source");
+			int target = link.getInt("target");
+			int value = link.getInt("value");
+			
+			//System.out.println(j+", "+source+", "+target+", "+value);
+			//System.out.println("target is "+characters.get(source));
+			
+			characters.get(source).addTarget( characters.get(target) );
+		}
+		//}
 	}
 
 }
